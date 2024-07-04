ebb4ea0cc2b65a5164650b1c196d55eb9757bae2
izako666 <izako.zayn@gmail.com>
2024-07-04 13:53:21 +0300

    added draw notification

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, json, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore, messaging
 
@@ -19,6 +19,64 @@ def send_notification():
         title = data.get('title')
         message = data.get('message')
         user_id = data.get('user_id')
+        message_id = data.get('message_id')
+        # Get user document from Firestore
+        user_ref = db.collection('users').document(user_id)
+        user_doc = user_ref.get()
+
+        if user_doc.exists:
+            # Get push_token
+            push_token = user_doc.get('push_token')
+            # Create the message
+            message = messaging.Message(
+                android=messaging.AndroidConfig(
+                    data={
+                           "default_vibrate_timings": "False",
+    "vibrate_timings": json.dumps([
+                "0.0s",
+                "0.2s",
+                "0.1s",
+                "0.2s",
+                "0.3s",
+                "0.1s",
+                "0.4s"
+     ]),
+     "message_type": "text/alert",
+     "message_id": message_id
+                    },
+                    priority='high',
+                       notification=messaging.AndroidNotification(
+                title=title,
+                body=message,
+                channel_id="alert_channel",
+                vibrate_timings_millis=[0, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000]  # Vibration pattern in milliseconds
+            ),
+                ),
+                notification=messaging.Notification(
+                    title=title,
+                    body=message,
+                ),
+                token=push_token,
+            )
+
+            # Send the message
+            response = messaging.send(message)
+            return jsonify({'status': 'success', 'message_id': response}), 200
+        else:
+            return jsonify({'status': 'error', 'message': 'User not found'}), 404
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
+@app.route('/send_notification_draw', endpoint='send_notification_draw', methods=['POST'])
+def send_notification():
+    try:
+        # Get data from request
+        data = request.get_json()
+        title = data.get('title')
+        message = data.get('message')
+        user_id = data.get('user_id')
+        message_id = data.get('message_id')
 
         # Get user document from Firestore
         user_ref = db.collection('users').document(user_id)
@@ -27,13 +85,12 @@ def send_notification():
         if user_doc.exists:
             # Get push_token
             push_token = user_doc.get('push_token')
-
             # Create the message
             message = messaging.Message(
                 android=messaging.AndroidConfig(
                     data={
-                           "default_vibrate_timings": False,
-    "vibrate_timings": [
+                           "default_vibrate_timings": "False",
+    "vibrate_timings": json.dumps([
                 "0.0s",
                 "0.2s",
                 "0.1s",
@@ -41,7 +98,9 @@ def send_notification():
                 "0.3s",
                 "0.1s",
                 "0.4s"
-     ],
+     ]),
+     "message_type": "text/draw",
+     "message_id": message_id
                     },
                     priority='high',
                        notification=messaging.AndroidNotification(
@@ -75,7 +134,7 @@ def send_notification():
         title = data.get('title')
         message = data.get('message')
         user_id = data.get('user_id')
-
+        message_id = data.get('message_id')
         # Get user document from Firestore
         user_ref = db.collection('users').document(user_id)
         user_doc = user_ref.get()
@@ -86,6 +145,11 @@ def send_notification():
 
             # Create the message
             message = messaging.Message(
+                data={
+                    "message_type": "text",
+                    "message_id": message_id
+
+                },
                 notification=messaging.Notification(
                     title=title,
                     body=message,
